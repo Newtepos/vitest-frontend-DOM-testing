650c0f74d0b1313d37e8102cd1a9d2dff6352e6b
Supakit <supakit.pnm@gmail.com>
2022-07-04 15:26:29 +0700

    Clean up DOM

--- util/dom.test.js
+++ util/dom.test.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-import { it, vi, expect } from "vitest";
+import { it, vi, expect, beforeEach } from "vitest";
 import { Window } from "happy-dom";
 
 import { showError } from "./dom";
@@ -11,14 +11,25 @@ const htmlDocumentContect = fs.readFileSync(htmlDocPath).toString();
 
 const window = new Window();
 const document = window.document;
-document.write(htmlDocumentContect);
 vi.stubGlobal("document", document);
 
-it("first test", () => {
+beforeEach(() => {
+  document.body.innerHTML = "";
+  document.write(htmlDocumentContect);
+});
+
+it("should add an error paragraph to the id='errors' element", () => {
   showError("test");
 
   const errorsEl = document.getElementById("errors");
   const errorParagraph = errorsEl.firstElementChild;
 
   expect(errorParagraph).not.toBeNull();
+});
+
+it("should not contain an error paragraph initially", () => {
+  const errorsEl = document.getElementById("errors");
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph).toBeNull();
 });
